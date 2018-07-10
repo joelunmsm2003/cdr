23bcaf2ec2e3e11e9c4bccc30b8a9c44f356752a
root <root@packer-debian-7-amd64.droplet.local>
2018-07-10 16:06:18 +0000

    filtros

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from daterange_filter.filter import DateRangeFilter
 
 # Register your models here.
 
@@ -8,13 +9,34 @@ from django.contrib import admin
 from .models import *
 from django.contrib import admin
 from django.contrib.admin.filters import RelatedOnlyFieldListFilter
-
+from daterange_filter.filter import DateRangeFilter
 from django.contrib.admin import AdminSite
 from django.utils.translation import ugettext_lazy
 
+
+class CallDateListFilter(DateRangeFilter):
+	# Human-readable title which will be displayed in the
+	# right admin sidebar just above the filter options.
+	title ='Citas'
+
+	# Parameter for the filter that will be used in the URL query.
+	parameter_name = 'Usuario'
+
+	# def lookups(self, request, model_admin):
+
+	#     return (
+	#         ('X', 'X'),
+	#     )
+
+	def queryset(self, request, queryset):
+
+		return queryset.all()
+
+
 @admin.register(Cdr)
 class CdrAdmin(admin.ModelAdmin):
-	list_display = ('acctid','calldate','clid','src','dst','dcontext','channel','dstchannel','lastapp','lastdata','duration','billsec','disposition','amaflags','accountcode','userfield','uniqueid','userfield','import_cdr')
+	list_display = ('calldate','clid','src','dst','dcontext','channel','dstchannel','lastapp','lastdata','duration','billsec','disposition','amaflags','accountcode','userfield','uniqueid','userfield','import_cdr')
+	list_filter = (CallDateListFilter,)
 	search_fields =('calldate',)
 
 
